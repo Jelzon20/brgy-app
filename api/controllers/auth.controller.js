b90d624e48028515a1b28f4c90bbd333e4bf86fd
jelzon20 <ja.holasca2@gmail.com>
2024-11-20 23:12:47 +0800

    Add error handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { firstName, lastName, email, password } = req.body;
     if (
         !email ||
@@ -14,8 +14,8 @@ export const signup = async (req, res) => {
         firstName === "" ||
         lastName === ""
       ) {
-        // next(errorHandler(400, 'All fields are required'));
-        return res.status(400).json({ message: 'All fields are required.'});
+        next(errorHandler(400, 'All fields are required'));
+        // return res.status(400).json({ message: 'All fields are required.'});
       }
 
       const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -31,6 +31,6 @@ export const signup = async (req, res) => {
         await newUser.save();
       res.json('Signup successful.');
       } catch (error) {
-        res.status(500).json({ message: error.message});
+        next(error)
       }
 }
--- api/index.js
+++ api/index.js
@@ -25,5 +25,12 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 
 
-
-//   .connect(process.env.MONGO)
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  })
+});
