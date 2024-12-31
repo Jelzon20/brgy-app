cd3b6af626a56964ef35e4710b6c347ed76a88d1
jelzon20 <ja.holasca2@gmail.com>
2024-12-31 18:24:12 +0800

    zzz 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import vitePluginRequire from "vite-plugin-require";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), vitePluginRequire.default()],
 })
